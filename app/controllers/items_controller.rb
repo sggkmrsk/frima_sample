e44e0fd95dfb53e962bda03be2de11e5450ec2d9
sggkmrsk <57355040+sggkmrsk@users.noreply.github.com>
2020-03-28 18:05:09 +0900

    rails g resource item

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,2 +1,39 @@
 class ItemsController < ApplicationController
+
+  def new
+    @item = Item.new
+    @images = @item.images.build
+  end
+
+  def create
+    @item = Item.new(item_params)
+    @item.save
+    redirect_to item_path(@item.id)
+  end
+
+  def edit
+    @item = Item.find(params[:id])
+  end
+
+  def show
+    @item = Item.find(params[:id])
+  end
+  def update
+    binding.pry
+    @item = Item.find(params[:id])
+    @item.update(item_update_params)
+  end
+
+  private
+  def item_params
+    params.require(:item).permit(
+      :title,
+      [images_attributes: [:image]])
+  end
+
+  def item_update_params
+    params.require(:item).permit(
+      :title,
+      [images_attributes: [:image, :_destroy, :id]])
+  end
 end
