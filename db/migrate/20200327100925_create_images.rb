96405c60a43d19d6a15369f2704b1e1f6d29de0b
sggkmrsk <57355040+sggkmrsk@users.noreply.github.com>
2020-03-28 18:06:50 +0900

    rails g model images

--- db/migrate/20200327100925_create_images.rb
+++ db/migrate/20200327100925_create_images.rb
@@ -1,7 +1,10 @@
 class CreateImages < ActiveRecord::Migration[5.2]
   def change
     create_table :images do |t|
-
+      # references
+      t.references :item,     null: false, foreign_key: true
+      # values
+      t.string     :image,    null: false
       t.timestamps
     end
   end
