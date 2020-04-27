80753b4f430ebce30ebf46a38d92ef496b2c5748
sggkmrsk <57355040+sggkmrsk@users.noreply.github.com>
2020-04-28 02:07:13 +0900

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,4 @@
 Rails.application.routes.draw do
   root 'items#new'
-  resources :items ,only: [:new,:create,:show,:edit,:update]
+  resources :items ,only: [:index,:new,:create,:show,:edit,:update]
 end
